3d327ea60eba942f5bfcfd83e5fadd295ac9d03c
menghuixipu <846083469@qq.com>
2020-04-10 23:19:34 +0800

    refs

---
+++ src/components/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login">
+    <div class="box">
+      <h2>登陆</h2>
+      <li ref="lidom">libiaoqian</li>
+      <button @click="subLogin">提交</button>
+      <button @click="getdom">refs</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    subLogin () {
+      console.log('点击了')
+      this.$router.push('/main')
+    },
+    getdom () {
+      console.log(this.$refs.lidom)
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.login {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .box {
+    width: 500px;
+    height: 400px;
+    border: 1px solid red;
+  }
+}
+</style>
---
+++ src/components/main.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="login">main</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
